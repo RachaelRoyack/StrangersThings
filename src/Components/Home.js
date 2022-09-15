cd2b4091ffbc3757cccf84d4fad83539a7a71899
RachaelRoyack <rachaelroyack@aol.com>
2022-09-15 17:20:57 -0400

    fixed homepage img

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
-import './Home.css'
+import './Home.css';
+import fleamarket from "../videos/fleamarket.jpeg"
 
 const Home = () => {
     return <div className="homeMain">
             <h1>Welcome To Stranger's Things!</h1>
             <p>...where one person's trash is another's treasure!</p>
-            <img src="../videos/fleamarket.jpeg" alt='flea market pic' />
+            <p><img className="homePageImg" src={fleamarket} alt='flea market pic' /></p>
             </div>
 }
 export default Home;
